ddeaf98c972b337d324fda21213e35dc68e2382b
Christian E. Hopps <chopps@gmail.com>
2017-06-27 08:01:32 -0400

    restore copy kernel to image dir

--- docker2img.sh
+++ docker2img.sh
@@ -85,6 +85,9 @@ mkdir -p ${mountpoint}
 
 create_root_from_docker $dockimg $mountpoint
 
+# Copy the kernel to imagedir
+${SUDO} cp /tmp/$iname/boot/vmlinuz* $imagedir/$kernel
+
 init_hosts_resolv $hosts_file
 
 # --------------
